e7876340fb8a62ca4d63868b91e44a8fb35e35d7
unknown <antonykibet03@gmail.com>
2022-07-31 18:27:44 +0300

    added items section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import Items from '../components/Items'
 
 function Home() {
   return (
     <div>
-
+        <Navbar/>
+        <Hero/>
+        <Items/>
     </div>
   )
 }
